f0db135fae5d6fe6753b042cde12aab52812de9a
Daniel Web <comercial@comercial.pontodasublimacao.com.br>
2026-06-30 16:39:10 -0300

    Centraliza configuracao do Supabase

---
+++ js/supabase-config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://pcugivsgiudlxhnuvttf.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_iMMKiY9-Rl95kESNDtHtYA_lFm-P1Ax';
+const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
